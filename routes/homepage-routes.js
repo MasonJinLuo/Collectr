85233db47eaa5ef51a74c27ab26d0c78ceb9b268
Stacy Ann Marshall <stacy.marshall@northwestern.edu>
2017-05-18 20:35:54 -0500

    added some lines to homepage-routes.js file

---
+++ routes/homepage-routes.js
@@ -0,0 +1,16 @@
+var db = require('../models');
+
+module.exports = function(app) {
+
+    app.get('/api/posts', function(req, res) {
+        db.Posts.findAll({
+
+        }).then(function(response) {
+            res.json(response);
+        });
+    });
+
+    app.get('/api/categories', function(req, res) {
+
+    });
+}
