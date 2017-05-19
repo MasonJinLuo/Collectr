db7cda356dacd66f8ee42dcd591a713dbebf071a
Stacy Ann Marshall <stacy.marshall@northwestern.edu>
2017-05-18 20:53:09 -0500

    removed index.js file

--- routes/homepage-routes.js
+++ routes/homepage-routes.js
@@ -2,14 +2,14 @@ var db = require('../models');
 
 module.exports = function(app) {
 
-    app.get('/api/posts', function(req, res) {
-        db.Posts.findAll({
+    // app.get('/api/posts', function(req, res) {
+    //     db.Posts.findAll({
 
-        }).then(function(response) {
-            res.json(response);
-        });
-    });
-    app.get('/api/categories', function(req, res) {
+    //     }).then(function(response) {
+    //         res.json(response);
+    //     });
+    // });
+    // app.get('/api/categories', function(req, res) {
 
-    });
+    // });
 }
