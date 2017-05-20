34e32e596684e6507585ae06b1affa6477351ddf
Stacy Ann Marshall <stacy.marshall@northwestern.edu>
2017-05-20 11:50:04 -0500

    minor tinkering, nothing of note

--- models/user.js
+++ models/user.js
@@ -1,5 +1,5 @@
 module.exports = function(sequelize, DataTypes) {
-    var User = sequelize.define("user", {
+    var User = sequelize.define("User", {
         email: {
             type: DataTypes.STRING,
             allowNull: false,
