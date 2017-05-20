aec874a3fa091072dac3c244db1e8834ead86b2c
Stacy Ann Marshall <stacy.marshall@northwestern.edu>
2017-05-20 13:15:24 -0500

    resolved a conflict

--- models/post.js
+++ models/post.js
@@ -1,77 +1,71 @@
 module.exports = function(sequelize, DataTypes) {
     var Post = sequelize.define("Post", {
-            post_id: { 
-                type: DataTypes.INTEGER,
-                primaryKey: true,
-                autoIncrement: true,
-                allowNull: false 
-            },
-            img_path: {
-                type: DataTypes.STRING,
-                allowNull: false,
-                validate: {
-                    len: [1]
-                }
-            },
-            description: {
-                type: DataTypes.TEXT,
-                allowNull: false,
-                validate: {
-                    len: [1]
-                }
-            },
-<<<<<<< HEAD
-=======
-
-            //Stacy: Added columns for tracking up and down votes on a post
-            upVote: {
-                type: DataTypes.INTEGER,
-                defaultValue: 0
-            },
-
-            downVote: {
-                type: DataTypes.INTEGER,
-                defaultValue: 0
-            },
-
->>>>>>> 30488fdecfa5a10dfafa1515fdd65dee83303436
-            createdAt: {
-                type: DataTypes.DATE,
-                field: 'createdAt',
-                defaultValue: sequelize.literal('NOW()')
-            },
-            updatedAt: {
-                type: DataTypes.DATE,
-                field: 'updatedAt',
-                defaultValue: sequelize.literal('NOW()')
+        post_id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+            allowNull: false
+        },
+        img_path: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1]
             }
         },
-        {
-            classMethods: {
-                associate: function(models) {
-                    this.belongsTo(models.User, {
-                        foreignKey: {
-                            name: 'owner_id',
-                            allowNull: false
-                        }
-                    });
-
-
-                    this.belongsTo(models.User, {
-                        foreignKey: {
-                            name: 'user_id',
-                            allowNull: false
-                        }
-                    });
-
-                    this.belongsTo(models.Category, {
-                        foreignKey: {
-                            name: 'category_id',
-                            allowNull: false
-                        }
-                    });
-                }
+        description: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+            validate: {
+                len: [1]
             }
-        });
+        },
+        //Stacy: Added columns for tracking up and down votes on a post
+        upVote: {
+            type: DataTypes.INTEGER,
+            defaultValue: 0
+        },
+
+        downVote: {
+            type: DataTypes.INTEGER,
+            defaultValue: 0
+        },
+        createdAt: {
+            type: DataTypes.DATE,
+            field: 'createdAt',
+            defaultValue: sequelize.literal('NOW()')
+        },
+        updatedAt: {
+            type: DataTypes.DATE,
+            field: 'updatedAt',
+            defaultValue: sequelize.literal('NOW()')
+        }
+    }, {
+        classMethods: {
+            associate: function(models) {
+                this.belongsTo(models.User, {
+                    foreignKey: {
+                        name: 'owner_id',
+                        allowNull: false
+                    }
+                });
+
+
+                this.belongsTo(models.User, {
+                    foreignKey: {
+                        name: 'user_id',
+                        allowNull: false
+                    }
+                });
+
+                this.belongsTo(models.Category, {
+                    foreignKey: {
+                        name: 'category_id',
+                        allowNull: false
+                    }
+                });
+            }
+        }
+    });
     return Post;
 };
--- models/post2tag.js
+++ models/post2tag.js
@@ -1,48 +1,47 @@
-module.exports = function(sequelize, DataTypes){
-	var Post2Tags = sequelize.define("Post2tag", {
-		post2tag_id: { 
+module.exports = function(sequelize, DataTypes) {
+    var Post2Tags = sequelize.define("Post2tag", {
+        post2tag_id: {
             type: DataTypes.INTEGER,
             primaryKey: true,
             autoIncrement: true,
-            allowNull: false 
+            allowNull: false
         },
-		tag_id: { 
+        tag_id: {
             type: DataTypes.INTEGER,
             allowNull: false
         },
-        post_id: { 
+        post_id: {
             type: DataTypes.INTEGER,
-            allowNull: false           
+            allowNull: false
         },
-		createdAt: {
-                type: DataTypes.DATE,
-                field: 'createdAt',
-                defaultValue: sequelize.literal('NOW()')
+        createdAt: {
+            type: DataTypes.DATE,
+            field: 'createdAt',
+            defaultValue: sequelize.literal('NOW()')
         },
-            updatedAt: {
-                type: DataTypes.DATE,
-                field: 'updatedAt',
-                defaultValue: sequelize.literal('NOW()')
+        updatedAt: {
+            type: DataTypes.DATE,
+            field: 'updatedAt',
+            defaultValue: sequelize.literal('NOW()')
         }
-	},
-	{
-		classMethods:{
-			associate: function(models){
-				this.belongsTo(models.Post, {
-					foreignKey:{
-						name: 'post_id', 
-						allowNull: false
-					}
-				});
+    }, {
+        classMethods: {
+            associate: function(models) {
+                this.belongsTo(models.Post, {
+                    foreignKey: {
+                        name: 'post_id',
+                        allowNull: false
+                    }
+                });
 
-				this.belongsTo(models.Tags, {
-					foreignKey:{
-						name: 'tag_id', 
-						allowNull: false
-					}
-				});
-			}
-		}
-	});
-	return Post2Tags;
+                this.belongsTo(models.Tags, {
+                    foreignKey: {
+                        name: 'tag_id',
+                        allowNull: false
+                    }
+                });
+            }
+        }
+    });
+    return Post2Tags;
 };
