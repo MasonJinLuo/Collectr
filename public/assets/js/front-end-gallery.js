78746cafa9557a698474f964262deda7d013ea2d
aflorend <aflorend@gmail.com>
2017-05-25 18:52:26 -0500

    Initial files for handlebars implementation.

---
+++ public/assets/js/front-end-gallery.js
@@ -0,0 +1,11 @@
+$("#menu-toggle").click(function(event) {
+    event.preventDefault();
+    $("#wrapper").toggleClass("toggled");
+});
+
+$("#gallery-scroll-left").click(function() {
+   $(this).siblings('.gallery-img-div').animate( { scrollLeft: '-=500' }, 500, 'easeOutQuad' );
+});
+$("#gallery-scroll-right").click(function() {
+   $(this).siblings('.gallery-img-div').animate( { scrollLeft: '+=500' }, 500, 'easeOutQuad' );
+});
