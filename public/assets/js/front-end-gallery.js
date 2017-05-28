f32d4141581a9de6295ff3b11cca8d9249fa2498
aflorend <aflorend@gmail.com>
2017-05-27 22:45:29 -0500

    Minor bug fixes for front-end galleries

--- public/assets/js/front-end-gallery.js
+++ public/assets/js/front-end-gallery.js
@@ -1,11 +1,13 @@
-$("#menu-toggle").click(function(event) {
-    event.preventDefault();
-    $("#wrapper").toggleClass("toggled");
-});
+$(document).ready(function() {
+  $("#menu-toggle").click(function(event) {
+      event.preventDefault();
+      $("#wrapper").toggleClass("toggled");
+  });
 
-$("#gallery-scroll-left").click(function() {
-   $(this).siblings('.gallery-img-div').animate( { scrollLeft: '-=500' }, 500, 'easeOutQuad' );
-});
-$("#gallery-scroll-right").click(function() {
-   $(this).siblings('.gallery-img-div').animate( { scrollLeft: '+=500' }, 500, 'easeOutQuad' );
+  $('.gallery-scroll-left').click(function() {
+     $(this).siblings('.gallery-img-div').animate( { scrollLeft: '-=500' }, 500, 'easeOutQuad' );
+  });
+  $('.gallery-scroll-right').click(function() {
+     $(this).siblings('.gallery-img-div').animate( { scrollLeft: '+=500' }, 500, 'easeOutQuad' );
+  });
 });
