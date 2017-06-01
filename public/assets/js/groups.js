cad25b930c988e0b9e115517aedc9069e0935db5
Anup Sawant <anupsawant@Anups-MacBook-Pro.local>
2017-05-31 20:23:11 -0500

    changes to groups.js

--- public/assets/js/groups.js
+++ public/assets/js/groups.js
@@ -11,6 +11,27 @@
     @desc: functions to group the posts
 */
 
+function getAllPostsInCategory(categoryID) {
 
+        var posts = [];
+
+        $.ajax({
+
+            method: 'GET',
+            url: '/api/category/' + categoryID
+
+        }).then(function(response) {
+
+            for (var i = 0; i < response.length; i++) { 
+
+                itemsInCategoryArray.push(response[i]);
+
+            }
+            
+        });
+
+        return itemsInCategoryArray;
+
+    };
 
 
