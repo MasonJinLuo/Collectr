76a5a6731c6a128885343f11f102a8ae78819fe0
Stacy Ann Marshall <stacy.marshall@northwestern.edu>
2017-06-01 18:02:36 -0500

    trying to fix collect modal to make it collect specific post info

--- public/assets/js/postModal.js
+++ public/assets/js/postModal.js
@@ -12,6 +12,30 @@ $(document).ready(function() {
     $(document).on('click', '.category-tab', categoryPage);
     $(document).on('click', '#collectPostSubmit', collectPostInfo);
 
+    $(document).on("click", "#collectClose", clearImage);
+    $(document).on("click", "#collectBtnModalClose", clearImage);
+
+    $(document).on("click", "#collectBtn", pinClick);
+
+    function clearImage() {
+        $('#collectPostForm').reset();
+        var description = "";
+        var category_id = 0;
+        var owner_id = 0;
+        var img_path = "";
+        var tags = "";
+    }
+
+    function pinClick(){
+        var owner = $(this).attr('data-owner');
+        var image = $(this).attr('data-img');
+        var id = $(this).attr('data-id');
+        console.log(owner);
+        console.log(image);
+        console.log(id);
+
+    }
+
     function collectPostInfo(event) {
 
         event.preventDefault();
@@ -21,89 +45,97 @@ $(document).ready(function() {
         var category_id = $(this).attr('data-category');
         var owner_id = $(this).attr('data-owner');
         var img_path = $(this).attr('data-img');
-
-        //the way I am passing the image path in an ajax call I only want the name of the image itself and not the whole path
-        var imageArray = img_path.split('/');
-        var imageArrayLength = imageArray.length;
-
-        var imageLocation = imageArray[imageArrayLength - 1];
-
-        //taking tags comma-delimited string and making a tags array
         var tags = $('#collectItemTags').val().trim().toLowerCase();
-        var tagArray = tags.split(',');
-        for (var i = 0; i < tagArray.length; i++) {
-            tagArray[i] = tagArray[i].trim();
-        }
 
-        var tagIdArray = [];
-        var oldTagNameArray = [];
-        var newTagNameArray = [];
+        if (!description) {
+            return alert("Please enter an item description");
+        } else if (!tags) {
+            return alert("Please enter item tags");
+        } else {
 
-        //checking to see if tags are currently in database
-        var promises = [];
+            //the way I am passing the image path in an ajax call I only want the name of the image itself and not the whole path
+            var imageArray = img_path.split('/');
+            var imageArrayLength = imageArray.length;
 
-        for (var i = 0; i < tagArray.length; i++) {
+            var imageLocation = imageArray[imageArrayLength - 1];
 
-            var tagName = tagArray[i];
-            var promise = $.ajax({
-                url: "/tags/" + tagName,
-                method: 'GET'
-            }).then(function(data) {
+            //taking tags comma-delimited string and making a tags array
 
-                //if the tags exist they are pushed to an array
-                if (data) {
-                    oldTagNameArray.push(data.name);
-                }
-
-            });
-            promises.push(promise);
-        }
-        $.when.apply(this, promises).then(function() {
-
-            //comparing original tag array to preexisting tag array
-            //push new tags to a new array
+            var tagArray = tags.split(',');
             for (var i = 0; i < tagArray.length; i++) {
-                console.log(tagArray[i]);
-                console.log(oldTagNameArray.indexOf(tagArray[i]));
-                if (oldTagNameArray.indexOf(tagArray[i]) == -1) {
-                    newTagNameArray.push(tagArray[i]);
-
-                }
+                tagArray[i] = tagArray[i].trim();
             }
 
-            //adding values of new tag array to tags table
-            for (var i = 0; i < newTagNameArray.length; i++) {
-                var newTagName = newTagNameArray[i];
-                $.ajax({
-                    url: "/tags/" + newTagName,
-                    method: "POST"
-                });
-            }
+            var tagIdArray = [];
+            var oldTagNameArray = [];
+            var newTagNameArray = [];
 
-            var promises2 = [];
+            //checking to see if tags are currently in database
+            var promises = [];
 
-            //get tag id values in an array for all of the tags
-            //will need tag ids to query and post Post2Tags model and associate the new post to these tags
             for (var i = 0; i < tagArray.length; i++) {
 
                 var tagName = tagArray[i];
-                var promise2 = $.ajax({
+                var promise = $.ajax({
                     url: "/tags/" + tagName,
                     method: 'GET'
                 }).then(function(data) {
 
-                    tagIdArray.push(data.id);
+                    //if the tags exist they are pushed to an array
+                    if (data) {
+                        oldTagNameArray.push(data.name);
+                    }
 
                 });
-                promises2.push(promise2);
+                promises.push(promise);
             }
-            $.when.apply(this, promises2).then(function() {
+            $.when.apply(this, promises).then(function() {
 
-                //call function to create new post passing in tagsID array and all info needed for a new post
-                createNewPost(tagIdArray, description, category_id, owner_id, imageLocation);
+                //comparing original tag array to preexisting tag array
+                //push new tags to a new array
+                for (var i = 0; i < tagArray.length; i++) {
+                    console.log(tagArray[i]);
+                    console.log(oldTagNameArray.indexOf(tagArray[i]));
+                    if (oldTagNameArray.indexOf(tagArray[i]) == -1) {
+                        newTagNameArray.push(tagArray[i]);
+
+                    }
+                }
+
+                //adding values of new tag array to tags table
+                for (var i = 0; i < newTagNameArray.length; i++) {
+                    var newTagName = newTagNameArray[i];
+                    $.ajax({
+                        url: "/tags/" + newTagName,
+                        method: "POST"
+                    });
+                }
+
+                var promises2 = [];
+
+                //get tag id values in an array for all of the tags
+                //will need tag ids to query and post Post2Tags model and associate the new post to these tags
+                for (var i = 0; i < tagArray.length; i++) {
+
+                    var tagName = tagArray[i];
+                    var promise2 = $.ajax({
+                        url: "/tags/" + tagName,
+                        method: 'GET'
+                    }).then(function(data) {
+
+                        tagIdArray.push(data.id);
+
+                    });
+                    promises2.push(promise2);
+                }
+                $.when.apply(this, promises2).then(function() {
+
+                    //call function to create new post passing in tagsID array and all info needed for a new post
+                    createNewPost(tagIdArray, description, category_id, owner_id, imageLocation);
+                });
+
             });
-
-        });
+        }
     }
 
     function createNewPost(tagIdArray, description, category_id, owner_id, imageLocation) {
@@ -135,7 +167,12 @@ $(document).ready(function() {
             }
 
             alert("Post Added!");
-            $('#collectPostForm')[0].reset();
+            $('#collectPostForm').reset();
+            var description = "";
+            var category_id = 0;
+            var owner_id = 0;
+            var img_path = "";
+            var tags = "";
             $('#openCollectModal').modal("hide");
 
         });
@@ -190,7 +227,7 @@ $(document).ready(function() {
     function categoryPage() {
 
         var categoryID = $(this).attr('id');
-        // alert('Category ID: ' + categoryID);
+
     }
 
 });
