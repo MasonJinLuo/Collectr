7085e5d78c8f77d8db26ec44f440fd64121170e3
Stacy Ann Marshall <stacy.marshall@northwestern.edu>
2017-06-02 07:00:06 -0500

    tweaks

--- routes/homepage-routes.js
+++ routes/homepage-routes.js
@@ -24,16 +24,6 @@ module.exports = function(app) {
             }]
         }).then(function(response) {
 
-            //Only put categories with content on the homepage
-            // var categoryHasPosts = [];
-            // for (var i = 0; i < response.length; i++) {
-            //     console.log(response[i].name + ': ' + response[i].Posts.length);
-
-            //     if (response[i].Posts.length > 0) {
-            //         categoryHasPosts.push(response[i]);
-            //     }
-            // }
-
             //Future Goal: Sort by popularity and render most popular first
             res.render('index', { category: response });
             // res.json(response);
