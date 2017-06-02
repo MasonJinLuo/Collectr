ffda1aa54ea69d49a2de14b53fef0e94cb1b5919
Stacy Ann Marshall <stacy.marshall@northwestern.edu>
2017-06-02 15:16:24 -0500

    seeds done

--- db/seeds.sql
+++ db/seeds.sql
@@ -17,35 +17,35 @@ insert into Categories (name) values ('Audio Gear');
 
 insert into users (email, password, image_path, description, interests) values ('GearHeadForLife', 'password', '/images/users/male1.jpg', 'Hey, I\'m Marcus. Born and raised in Los Angelos California, dreaming of the day I can move to New York City. I have a massive collection of maps and I love anything historical. Hit me up!!', '2,7,14');
 insert into users (email, password, image_path, description, interests) values ('Jeff_Is_Da_Man', 'password', '/images/users/male2.jpg', 'I have a passion for music and the finer things in life. My favorites are Frank Sinatra, Sammy David Jr, and John Legend. I love to listen to old tunes and be carried away by nostalgia', '6,10,13,14,15');
-insert into users (email, password, image_path, description, interests) values ('MusicFan_79', 'password', '/images/users/male3.jpg', '', '1,3,4');
-insert into users (email, password, image_path, description, interests) values ('Wookie1977', 'password', '/images/users/male4.jpg', '', '5,6,14');
+insert into users (email, password, image_path, description, interests) values ('MusicBookFan_79', 'password', '/images/users/male3.jpg', 'My user name pretty much says it all. I enjoy fresh beats and great reads. I\'ve been living a blessed life so far, and I\'m looking to find some people with similar collection interests!', '1,3,4');
+insert into users (email, password, image_path, description, interests) values ('Wookie1977', 'password', '/images/users/male4.jpg', 'Ready are you? What know you of ready? For eight hundred years have I trained Jedi. My own counsel will I keep on who is to be trained. A Jedi must have the deepest commitment, the most serious mind. This one, a long time have I watched. All his life has he looked away… to the future, to the horizon', '5,6,14');
 
-insert into users (email, password, image_path, description, interests) values ('AllAboutThatBass', 'password', '/images/users/female1.jpg', '', '5,6,8');
-insert into users (email, password, image_path, description, interests) values ('Tiny_Dancer_2', 'password', '/images/users/female2.jpg', '', '1,2,3');
-insert into users (email, password, image_path, description, interests) values ('GreatGatsby', 'password', '/images/users/female3.jpg', '', '9,10,11,12');
-insert into users (email, password, image_path, description, interests) values ('NyanCat', 'password', '/images/users/female4.jpg', '', '15,5,4');
-
-
-insert into posts (img_path, description, owner_id, user_id, category_id) values ('/images/postImages/stamp1.png', 'Argentina Stamp!', 1, 1, 1);
-insert into posts (img_path, description, owner_id, user_id, category_id) values ('/images/postImages/map1.jpg', 'From My Original Collection', 1, 1, 2);
-insert into posts (img_path, description, owner_id, user_id, category_id) values ('/images/postImages/guitar1.jpg', 'My Fender, My Pride and Joy', 2, 2, 13);
-insert into posts (img_path, description, owner_id, user_id, category_id) values ('/images/postImages/audioGear1.jpg', 'Set Up Down At WX97', 2, 2, 16);
-
-insert into posts (img_path, description, owner_id, user_id, category_id) values ('/images/postImages/coin1.jpg', 'Leo V, Dumbarton Oaks', 3, 3, 3);
-insert into posts (img_path, description, owner_id, user_id, category_id) values ('/images/postImages/book1.jpg', '<3 Forever Favorite', 3, 3, 4);
-insert into posts (img_path, description, owner_id, user_id, category_id) values ('/images/postImages/card1.jpg', 'Nidoran (f)', 4, 4, 5);
-insert into posts (img_path, description, owner_id, user_id, category_id) values ('/images/postImages/videoGame1.jpg', 'Sad To Part With This', 4, 4, 6);	
+insert into users (email, password, image_path, description, interests) values ('AllAboutThatBass', 'password', '/images/users/female1.jpg', 'I\'m a Minnesota girl who loves spending time with her friends and family. I have three fur babies (Huskies!) who accompany my husband and I on every camping trip. I\'m a nerd at heart and I enjoy all things Marvel!', '5,6,8');
+insert into users (email, password, image_path, description, interests) values ('Tiny_Dancer_2', 'password', '/images/users/female2.jpg', 'Major history buff Veronica here. I very much enjoy Eurpoean history and all things related to the Tang Dynasty', '1,2,3');
+insert into users (email, password, image_path, description, interests) values ('GreatGatsby', 'password', '/images/users/female3.jpg', 'Hello all, I\'m Camile. I am a personal shopper at Nordstrom and I enjoy getting to spend my time surrounded by high fashion and cool people. In my free time I am lead guitarist for my neighborhood band \'Groots Galaxy\'', '9,10,11,12');
+insert into users (email, password, image_path, description, interests) values ('NyanCatTsum', 'password', '/images/users/female4.jpg', 'Total music nerd here! I have a big personality and I love smiling (Smiling\'s my favorite). Let\'s be friends!!!!', '15,5,4');
 
 
-insert into posts (img_path, description, owner_id, user_id, category_id) values ('/images/postImages/actionFigure1.jpg', 'Superman Making His Rounds', 5, 5, 7);
-insert into posts (img_path, description, owner_id, user_id, category_id) values ('/images/postImages/boardGame1.jpg', 'Perfect Ladies\' Night!! <3', 5, 5, 8);
-insert into posts (img_path, description, owner_id, user_id, category_id) values ('/images/postImages/shoe1.jpg', 'All About These Sperrys!', 6, 6, 9);
-insert into posts (img_path, description, owner_id, user_id, category_id) values ('/images/postImages/hat1.jpg', 'My Sister\'s New Hat', 6, 6, 10);
+insert into posts (img_path, description, upVote, downVote, owner_id, user_id, category_id) values ('/images/postImages/stamp1.png', 'Argentina Stamp!', 34, 1, 1, 1, 1);
+insert into posts (img_path, description, upVote, downVote, owner_id, user_id, category_id) values ('/images/postImages/map1.jpg', 'From My Original Collection', 187, 5, 1, 1, 2);
+insert into posts (img_path, description, upVote, downVote, owner_id, user_id, category_id) values ('/images/postImages/guitar1.jpg', 'My Fender, My Pride and Joy', 200, 3, 2, 2, 13);
+insert into posts (img_path, description, upVote, downVote, owner_id, user_id, category_id) values ('/images/postImages/audioGear1.jpg', 'Set Up Down At WX97', 35, 6, 2, 2, 16);
 
-insert into posts (img_path, description, owner_id, user_id, category_id) values ('/images/postImages/watch1.jpg', 'Love That Wrapping Style', 7, 7, 11);
-insert into posts (img_path, description, owner_id, user_id, category_id) values ('/images/postImages/bag1.jpg', 'Saw This At Nordstrom', 7, 7, 12);
-insert into posts (img_path, description, owner_id, user_id, category_id) values ('/images/postImages/recordings1.jpg', 'Best Album of All Time', 8, 8, 14);
-insert into posts (img_path, description, owner_id, user_id, category_id) values ('/images/postImages/memorabilia1.jpg', 'Violent Femmes Rock My Socks', 8, 8, 15);	
+insert into posts (img_path, description, upVote, downVote, owner_id, user_id, category_id) values ('/images/postImages/coin1.jpg', 'Leo V, Dumbarton Oaks', 10, 2, 3, 3, 3);
+insert into posts (img_path, description, upVote, downVote, owner_id, user_id, category_id) values ('/images/postImages/book1.jpg', '<3 Forever Favorite', 176, 4, 3, 3, 4);
+insert into posts (img_path, description, upVote, downVote, owner_id, user_id, category_id) values ('/images/postImages/card1.jpg', 'Nidoran (f)', 75, 1, 4, 4, 5);
+insert into posts (img_path, description, upVote, downVote, owner_id, user_id, category_id) values ('/images/postImages/videoGame1.jpg', 'Sad To Part With This', 350, 45, 4, 4, 6);	
+
+
+insert into posts (img_path, description, upVote, downVote, owner_id, user_id, category_id) values ('/images/postImages/actionFigure1.jpg', 'Superman Making His Rounds', 176, 14, 5, 5, 7);
+insert into posts (img_path, description, upVote, downVote, owner_id, user_id, category_id) values ('/images/postImages/boardGame1.jpg', 'Perfect Ladies\' Night!! <3', 15, 1, 5, 5, 8);
+insert into posts (img_path, description, upVote, downVote, owner_id, user_id, category_id) values ('/images/postImages/shoe1.jpg', 'All About These Sperrys!', 3, 0, 6, 6, 9);
+insert into posts (img_path, description, upVote, downVote, owner_id, user_id, category_id) values ('/images/postImages/hat1.jpg', 'My Sister\'s New Hat', 75, 2, 6, 6, 10);
+
+insert into posts (img_path, description, upVote, downVote, owner_id, user_id, category_id) values ('/images/postImages/watch1.jpg', 'Love That Wrapping Style', 3, 0, 7, 7, 11);
+insert into posts (img_path, description, upVote, downVote, owner_id, user_id, category_id) values ('/images/postImages/bag1.jpg', 'Saw This At Nordstrom', 63, 4, 7, 7, 12);
+insert into posts (img_path, description, upVote, downVote, owner_id, user_id, category_id) values ('/images/postImages/recordings1.jpg', 'Best Album of All Time', 243, 34, 8, 8, 14);
+insert into posts (img_path, description, upVote, downVote, owner_id, user_id, category_id) values ('/images/postImages/memorabilia1.jpg', 'Violent Femmes Rock My Socks', 134, 21, 8, 8, 15);	
 
 insert into tags (name) values ('cute');
 insert into tags (name) values ('cool');
