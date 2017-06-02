fdd34734676c854c9cac659d392e50161842189d
Stacy Ann Marshall <stacy.marshall@northwestern.edu>
2017-06-01 20:57:44 -0500

    fixed bug in index file, removed old packages

--- server.js
+++ server.js
@@ -15,16 +15,11 @@ var PORT = process.env.PORT || 8080;
 // Requiring our models for syncing
 var collectrdb = require("./models");
 
-var hbs = exphbs.create({
-
-});
-
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
-// app.use(methodOverride("_method"));
 
 //Sets up handlebars as view engine
 app.engine("handlebars", exphbs({
